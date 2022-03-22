3cd1f71f5c63ab288e179698d1e9f623c9d773a1
benk <benkalegin@users.noreply.github.com>
2022-03-21 23:18:51 -0400

    default ports

--- react/src/ClassDiagram/Node.tsx
+++ react/src/ClassDiagram/Node.tsx
@@ -1,4 +1,5 @@
 import styles from './Node.module.scss';
+import classNames from 'classnames';
 
 
 export const Node = () =>
@@ -7,9 +8,9 @@ export const Node = () =>
          style={{top: 50, left: 50}}>
         <div className={styles.defaultNode}>
             Hello
-            <div className={styles.port_bottom} data-port-id="72f55820-671a-4142-bb1d-523af53639af"/>
-            <div className="port top  default" data-port-id="4b066b74-53a8-4bb8-b077-a135cf3cd346"/>
-            <div className="port left  default" data-port-id="c04cefc3-a148-4566-9fa9-022c8c89d402"/>
-            <div className="port right has-links default" data-port-id="1e57e6aa-f6ea-4514-ba5d-88ee6c37c125"/>
+            <div className={classNames(styles.port, styles.bottom)} data-port-id="72f55820-671a-4142-bb1d-523af53639af"/>
+            <div className={classNames(styles.port, styles.top)} data-port-id="4b066b74-53a8-4bb8-b077-a135cf3cd346"/>
+            <div className={classNames(styles.port, styles.left)}  data-port-id="c04cefc3-a148-4566-9fa9-022c8c89d402"/>
+            <div className={classNames(styles.port, styles.right)} data-port-id="1e57e6aa-f6ea-4514-ba5d-88ee6c37c125"/>
         </div>
     </div>;
