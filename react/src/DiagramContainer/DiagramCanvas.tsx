216e272faaa831d1dcfd47a755da6b341908e992
benk <benkalegin@users.noreply.github.com>
2022-10-18 22:48:57 -0400

    added konva ref

--- react/src/DiagramContainer/DiagramCanvas.tsx
+++ react/src/DiagramContainer/DiagramCanvas.tsx
@@ -1,11 +1,94 @@
 import styles from './DiagramCanvas.module.scss';
 import {Node} from "../ClassDiagram/Node";
-import React, {MouseEventHandler, useReducer, useState} from "react";
+import React, {MouseEventHandler, RefObject, useReducer, useState} from "react";
 import {Link} from "../ClassDiagram/Link";
+import {Stage, Layer, Rect, Transformer} from 'react-konva';
 import {InteractionDispatch} from "./InteractionDispatch";
 import {ClassDiagramState, ClassDiagramViewState, NodeState, Port, PortPosition} from "../ClassDiagram/Models";
 import {DiagramElement} from "../Common/Model";
 import {cloudDiagramReducer} from "../ClassDiagram/Reducer";
+import Konva from "konva";
+interface RectAttrs {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    fill: string;
+    id: string;
+}
+
+interface RectProps {
+    shapeProps: RectAttrs;
+    isSelected: boolean;
+    onSelect: (evt: Konva.KonvaEventObject<MouseEvent>) => void;
+    onChange: (newAttrs: RectAttrs) => void;
+}
+
+const Rectangle = (props: RectProps) => {
+    const shapeRef: RefObject<Konva.Rect> = React.useRef(null);
+    const trRef: RefObject<Konva.Transformer> = React.useRef(null);
+
+    React.useEffect(() => {
+        if (props.isSelected) {
+            // we need to attach transformer manually
+            // @ts-ignore
+            trRef.current.nodes([shapeRef.current]);
+            // @ts-ignore
+            trRef.current.getLayer().batchDraw();
+        }
+    }, [props.isSelected]);
+
+    return (
+        <React.Fragment>
+            <Rect
+                onClick={props.onSelect}
+                ref={shapeRef}
+                {...props.shapeProps}
+                draggable
+                onDragEnd={(e) => {
+                    props.onChange({
+                        ...props.shapeProps,
+                        x: e.target.x(),
+                        y: e.target.y(),
+                    });
+                }}
+                onTransformEnd={() => {
+                    // transformer is changing scale of the node
+                    // and NOT its width or height
+                    // but in the store we have only width and height
+                    // to match the data better we will reset scale on transform end
+                    const node = shapeRef.current!;
+                    const scaleX = node.scaleX();
+                    const scaleY = node.scaleY();
+
+                    // we will reset it back
+                    node.scaleX(1);
+                    node.scaleY(1);
+                    props.onChange({
+                        ...props.shapeProps,
+                        x: node.x(),
+                        y: node.y(),
+                        // set minimal value
+                        width: Math.max(5, node.width() * scaleX),
+                        height: Math.max(node.height() * scaleY),
+                    });
+                }}
+            />
+            {props.isSelected && (
+                <Transformer
+                    ref={trRef}
+                    boundBoxFunc={(oldBox, newBox) => {
+                        // limit resize
+                        if (newBox.width < 5 || newBox.height < 5) {
+                            return oldBox;
+                        }
+                        return newBox;
+                    }}
+                />
+            )}
+        </React.Fragment>
+    );
+};
 
 function getDefaultDiagramState(): ClassDiagramState {
     let port1 = {position: PortPosition.Right};
@@ -93,19 +176,79 @@ export function DiagramCanvas() {
             handler.onMouseUp(element, e.clientX, e.clientY);
     }
 
-    return <div className={styles.canvas}
-                onMouseDown={(e) => handleMouseDown(e)}
-                onMouseUp={(e) => handleMouseUp(e)}
-                onMouseMove={(e) => handleMouseMove(e)}>
-        <svg className={styles.svgLayer} scale="1">
-            {diagram.Links.map((link, index) => {
-                return <Link key={index} {...link} />
-            })}
-        </svg>
-        <div className={styles.htmlLayer}>
-            {diagram.Nodes.map((node, index) => {
-                return <Node key={index} {...node} />
-            })}
-        </div>
-    </div>;
+    const initialRectangles: RectAttrs[] = [
+        {
+            x: 10,
+            y: 10,
+            width: 100,
+            height: 100,
+            fill: 'red',
+            id: 'rect1',
+        },
+        {
+            x: 150,
+            y: 150,
+            width: 100,
+            height: 100,
+            fill: 'green',
+            id: 'rect2',
+        },
+    ];
+
+
+    const [rectangles, setRectangles] = React.useState(initialRectangles);
+    const [selectedId, selectShape] = React.useState<string | null>(null);
+
+    const checkDeselect = (e: Konva.KonvaEventObject<MouseEvent>) => {
+        // deselect when clicked on empty area
+        const clickedOnEmpty = e.target === e.target.getStage();
+        if (clickedOnEmpty) {
+            selectShape(null);
+        }
+    };
+
+    return (
+        <Stage
+            width={window.innerWidth}
+            height={window.innerHeight}
+            onMouseDown={checkDeselect}
+        >
+            <Layer>
+                {rectangles.map((rect, i) => {
+                    return (
+                        <Rectangle
+                            key={i}
+                            shapeProps={rect}
+                            isSelected={rect.id === selectedId}
+                            onSelect={() => {
+                                selectShape(rect.id);
+                            }}
+                            onChange={(newAttrs: RectAttrs) => {
+                                const rects = rectangles.slice();
+                                rects[i] = newAttrs;
+                                setRectangles(rects);
+                            }}
+                        />
+                    );
+                })}
+            </Layer>
+        </Stage>
+    );
+
+
+    // return <div className={styles.canvas}
+    //             onMouseDown={(e) => handleMouseDown(e)}
+    //             onMouseUp={(e) => handleMouseUp(e)}
+    //             onMouseMove={(e) => handleMouseMove(e)}>
+    //     <svg className={styles.svgLayer} scale="1">
+    //         {diagram.Links.map((link, index) => {
+    //             return <Link key={index} {...link} />
+    //         })}
+    //     </svg>
+    //     <div className={styles.htmlLayer}>
+    //         {diagram.Nodes.map((node, index) => {
+    //             return <Node key={index} {...node} />
+    //         })}
+    //     </div>
+    // </div>;
 }
