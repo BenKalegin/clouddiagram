2ce15208f881e2280b0f3588a411d386063056c3
benk <benkalegin@users.noreply.github.com>
2022-10-21 23:37:28 -0400

    ports on the edges and fix text click through

--- react/src/ClassDiagram/Node.tsx
+++ react/src/ClassDiagram/Node.tsx
@@ -1,8 +1,6 @@
-import styles from './Node.module.scss';
-import classNames from 'classnames';
-import {NodeState, Port, PortPosition} from "./Models";
+import {Bounds, Coordinate, NodeState, Port, PortPosition} from "./Models";
 import React, {RefObject} from "react";
-import {Circle, Rect, Text, Transformer} from "react-konva";
+import {Circle, Group, Label, Rect, Text, Transformer} from "react-konva";
 import Konva from "konva";
 
 export interface NodeProps {
@@ -14,21 +12,31 @@ export interface NodeProps {
 }
 
 export const Node = (props: NodeProps) => {
-    const portPositionClass = (pos: PortPosition) => {
-        switch (pos) {
-            case PortPosition.Top:
-                return styles.top;
-            case PortPosition.Bottom:
-                return styles.bottom;
-            case PortPosition.Left:
-                return styles.left;
-            case PortPosition.Right:
-                return styles.right;
-        }
-    };
 
-    const portClasses = (port: Port) => {
-        return classNames(styles.port, portPositionClass(port.position));
+    const portPos = (port: Port): Coordinate => {
+        const node: Bounds = props.node;
+        switch (port.position) {
+            case PortPosition.Top:
+                return {
+                    x: node.x + node.width / 2,
+                    y: node.y
+                }
+            case PortPosition.Bottom:
+                return {
+                    x: node.x + node.width / 2,
+                    y: node.y + node.height
+                }
+            case PortPosition.Left:
+                return {
+                    x: node.x,
+                    y: node.y + node.height / 2
+                }
+            case PortPosition.Right:
+                return {
+                    x: node.x + node.width,
+                    y: node.y + node.height / 2
+                };
+        }
     };
 
     const shapeRef: RefObject<Konva.Rect> = React.useRef(null);
@@ -44,73 +52,78 @@ export const Node = (props: NodeProps) => {
         }
     }, [props.isSelected]);
 
-    console.log( "Node: " + props.node.id +  " selected " + props.isSelected);
+    console.log("Node: " + props.node.id + " selected " + props.isSelected);
 
     return (
         <React.Fragment>
-            <Rect
-                onClick={props.onSelect}
-                ref={shapeRef}
-                fill={"red"}
-                {...props.node}
-                cornerRadius={10}
-                draggable
-                onDragEnd={(e) => {
-                    props.onChange({...props.node, x: e.target.x(), y: e.target.y()});
-                }}
-                onTransformEnd={() => {
-                    // transformer is changing scale of the node
-                    // and NOT its width or height
-                    // but in the store we have only width and height
-                    // to match the data better we will reset scale on transform end
-                    const n = shapeRef.current!;
-                    const scaleX = n.scaleX();
-                    const scaleY = n.scaleY();
-
-                    // we will reset it back
-                    n.scaleX(1);
-                    n.scaleY(1);
-                    props.onChange({
-                        ...props.node,
-                        x: n.x(),
-                        y: n.y(),
-                        // set minimal value
-                        width: Math.max(5, n.width() * scaleX),
-                        height: Math.max(n.height() * scaleY),
-                    });
-                }}
-            />
-            <Text
-                x={props.node.x}
-                y={props.node.y}
-                width={props.node.width}
-                height={props.node.height}
-                align={"center"}
-                verticalAlign={"middle"}
-                text={"Hello"}
-            />
-            {props.isSelected && (
-                <Transformer
-                    ref={trRef}
-                    boundBoxFunc={(oldBox, newBox) => {
-                        // limit resize
-                        if (newBox.width < 5 || newBox.height < 5) {
-                            return oldBox;
-                        }
-                        return newBox;
+                <Rect
+                    onClick={props.onSelect}
+                    ref={shapeRef}
+                    fill={"red"}
+                    {...props.node}
+                    cornerRadius={10}
+                    draggable
+                    onDragEnd={(e) => {
+                        props.onChange({...props.node, x: e.target.x(), y: e.target.y()});
                     }}
+                    onTransformEnd={() => {
+                        // transformer is changing scale of the node
+                        // and NOT its width or height
+                        // but in the store we have only width and height
+                        // to match the data better we will reset scale on transform end
+                        const n = shapeRef.current!;
+                        const scaleX = n.scaleX();
+                        const scaleY = n.scaleY();
+
+                        // we will reset it back
+                        n.scaleX(1);
+                        n.scaleY(1);
+                        props.onChange({
+                            ...props.node,
+                            x: n.x(),
+                            y: n.y(),
+                            // set minimal value
+                            width: Math.max(5, n.width() * scaleX),
+                            height: Math.max(n.height() * scaleY),
+                        });
+                    }}>
+                </Rect>
+                {props.isSelected && (
+                    <Transformer
+                        ref={trRef}
+                        boundBoxFunc={(oldBox, newBox) => {
+                            // limit resize
+                            if (newBox.width < 5 || newBox.height < 5) {
+                                return oldBox;
+                            }
+                            return newBox;
+                        }}
+                    />
+                )}
+                <Text
+                    x={props.node.x}
+                    y={props.node.y}
+                    fontSize={14}
+                    width={props.node.width}
+                    height={props.node.height}
+                    align={"center"}
+                    verticalAlign={"middle"}
+                    text={"Hello"}
+                    draggable={false}
+                    listening={false}
+                    preventDefault={true}
+                    // https://stackoverflow.com/questions/49692354/is-there-a-way-to-define-text-inside-a-konva-rect-class-by-using-only-konva-rect
+                    // Use Konva.Label
+                    // Create a draggable group with rectangle and text inside
+                    // Create one custom shape. Draw rectangle and text inside sceneFunc
                 />
-            )}
-            {props.node.ports.map((port, index) =>
-                <Circle
-                    x={props.node.x + (port.position === PortPosition.Left ? 0 : props.node.width)}
-                    y={props.node.y + (port.position === PortPosition.Top ? 0 : props.node.height)}
-                    radius={5}
-                    fill={"green"}
-                    key={index}
-                    {...port}
-                    className={portClasses(port)}
-                />)}
+                {props.node.ports.map((port, index) =>
+                    <Circle
+                        {...portPos(port)}
+                        radius={5}
+                        fill={"green"}
+                        key={index}
+                    />)}
 
         </React.Fragment>)
 };
