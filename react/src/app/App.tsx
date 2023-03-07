27cc78d14bb746f47530681c3a9bbcea43221edc
benk <benkalegin@users.noreply.github.com>
2023-03-06 23:56:27 -0500

    adding props drawer

--- react/src/app/App.tsx
+++ react/src/app/App.tsx
@@ -2,24 +2,154 @@ import React from 'react';
 import './App.css';
 import {Toolbox} from "../features/toolbox/Toolbox";
 import {DiagramTabs} from "../features/diagramTabs/DiagramTabs";
-import {Divider, Stack, Typography} from "@mui/material";
-import {FilterDrama}  from '@mui/icons-material';
+import {
+    Box,
+    Button,
+    CssBaseline,
+    Divider,
+    Drawer,
+    IconButton,
+    Stack,
+    styled,
+    Typography,
+    useTheme
+} from "@mui/material";
+import List from "@mui/material/List";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+const drawerWidth = 240;
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
+    open?: boolean;
+}>(({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+    }),
+    marginRight: -drawerWidth,
+    ...(open && {
+        transition: theme.transitions.create("margin", {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen
+        }),
+        marginRight: 0
+    })
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-start"
+}));
 
 export const App = () => {
     // browser: browserSlice.reducer,
     // toolbox: toolboxSlice.reducer,
     // package: packageSlice.reducer
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+
     return (
-        <Stack direction="column">
-            <Stack direction="row">
-                <FilterDrama/>
-                <Typography>Cloud Diagram</Typography>
-            </Stack>
-            <Stack direction="row">
-                <Toolbox/>
-                <Divider/>
-                <DiagramTabs/>
-            </Stack>
-        </Stack>
+        <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            {/* <Toolbar>
+          <Typography variant="h6" noWrap sx={{ flexGrow: 1 }} component="div">
+            Persistent drawer
+          </Typography>
+        </Toolbar> */}
+            <Main open={open}>
+                <Stack direction="row">
+                    <Typography>Cloud Diagram</Typography>
+                    <Button
+                        onClick={handleDrawerOpen}
+                        sx={{
+                            position: "absolute",
+                            top: 5,
+                            right: 5,
+                            borderRadius: "50%"
+                        }}
+                    >
+                        {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </Button>
+                </Stack>
+                {/*<DrawerHeader />*/}
+                <Stack direction="column">
+                    <Stack direction="row">
+                        <Toolbox/>
+                        <Divider/>
+                        <DiagramTabs/>
+                    </Stack>
+                </Stack>
+            </Main>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    "& .MuiDrawer-paper": {
+                        width: drawerWidth
+                    }
+                }}
+                variant="persistent"
+                anchor="right"
+                open={open}
+            >
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === "rtl" ? (
+                            <ChevronLeftIcon />
+                        ) : (
+                            <ChevronRightIcon />
+                        )}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider />
+                <List>
+                    {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                </ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider />
+                <List>
+                    {["All mail", "Trash", "Spam"].map((text, index) => (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                </ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+        </Box>
     );
 };
